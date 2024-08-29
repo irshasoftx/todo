d1aec93c014b8d314fc515bcf340aa7da3c51cb6
githab <irshasoftzone@gmail.com>
2024-08-29 22:48:47 +0530

    created a page

--- pages/About.py
+++ pages/About.py
@@ -1,64 +1,60 @@
 import streamlit as st
 
+# Add your logo at the top
+st.image("logo.png", width=200)
 
-st.image("logo.png", width=200)  # Add your logo here
-# Using the custom CSS classes
+# Title with custom CSS class
 st.markdown('<div class="title">Welcome to Irshasoft ToDo App 1.0</div>', unsafe_allow_html=True)
 
-st.write(""  )
-st.write(""  )
-st.markdown('<h2 style ="color:red;">Irshasoft International</h2>',unsafe_allow_html=True)
-st.markdown("Customer Care: http://wa.me/919562777796")
-st.write("Email: irshasoft@gmail.com")
-st.write("Instagram: https://www.instagram.com/irshasoftx/")
-st.write("" )
-st.write( "" )
-st.write( "" )
-st.write( "" )
-st.write( "" )
-st.write( "" )
-st.title("Useful Links")
-st.write("You Wanna Watch Top Movies: https://www.imdb.com/list/ls055483029/")
+# Adding space between elements
+st.write("\n\n")
 
-st.write("" )
-st.write( "" )
-st.write( "" )
-st.write( "" )
-st.write( "" )
-st.write( "" )
+# Subtitle styled in red
+st.markdown('<h2 style="color:red;">Irshasoft International</h2>', unsafe_allow_html=True)
+
+# Contact information
+st.markdown("[Customer Care: http://wa.me/919562777796](http://wa.me/919562777796)")
+st.write("Email: irshasoft@gmail.com")
+st.write("[Instagram: https://www.instagram.com/irshasoftx/](https://www.instagram.com/irshasoftx/)")
+
+# Adding space
+st.write("\n\n")
+
+# Useful Links Section
+st.title("Useful Links")
+st.write("[You Wanna Watch Top Movies? Click Here](https://www.imdb.com/list/ls055483029/)")
+
+# Adding space
+st.write("\n\n")
+
+# Book gallery
+st.title("Book Gallery")
 
 # Sample images - replace with your own images or URLs
 image_urls = [
-    "20927404112712-Capture.png",
-    "48847571765852-oru-pa.jpg",
-    "bk_9852.jpg",
-    "Vismayam.jpg",
-    "41BUiQsNbqL._SY466_.jpg",
-    "714711.jpg",
+    "./pages/20927404112712-Capture.png",
+    "./pages/48847571765852-oru-pa.jpg",
+    "./pages/bk_9852.jpg",
+    "./pages/Vismayam.jpg",
+    "./pages/41BUiQsNbqL._SY466_.jpg",
+    "./pages/714711.jpg",
 ]
 
 # Number of columns in the gallery
 num_cols = 3
 
-# Create a grid layout
+# Create a grid layout for the images
 cols = st.columns(num_cols)
 
-# Display images in the grid
-# for index, img in enumerate(image_urls):
-#     cols[index % num_cols].image(img, use_column_width=True)
-
+# Display images in a grid with captions
 for index, img in enumerate(image_urls):
     with cols[index % num_cols]:
         st.image(img, caption=f"Book {index+1}", use_column_width=True)
 
+# Contact information for the books
+st.write("If you want these Books, Please Contact: +91 9562 77 77 96")
 
-
-
-st.write("if you want those Books Please Contact:  +91 9562 77 77 96 ")
-
-import streamlit as st
-
-# Injecting CSS using st.markdown
+# Injecting CSS for additional styling
 st.markdown(
     """
     <style>
@@ -68,7 +64,6 @@ st.markdown(
         font-weight: bold;
         text-align: center;
     }
-    
     .sidebar .sidebar-content {
         background-color: #f0f0f0;
     }
@@ -77,8 +72,5 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-
-
-
-# Sidebar styling (using CSS class defined above)
+# Sidebar styling
 st.sidebar.write("This is the sidebar with custom background color.")
